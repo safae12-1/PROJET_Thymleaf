744e3ffcebcce82db258977185681690e31bff1c
safae12-1 <124156186+safae12-1@users.noreply.github.com>
2023-12-07 22:15:56 +0100

    first commit
    07/12/2023

---
+++ Projet_Thymleaf/src/main/java/com/example/projet_thymleaf/ProjetThymleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_thymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetThymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetThymleafApplication.class, args);
+	}
+
+}
---
+++ Projet_Thymleaf/src/test/java/com/example/projet_thymleaf/ProjetThymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_thymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetThymleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
